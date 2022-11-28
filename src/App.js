9749484cde8cc59e5c55e0a615fd33e7d187366a
damnge <dam.grela@gmail.com>
2022-11-28 22:34:33 +0000

    cinema and trends

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.css";
+import CinemaAndTrends from "./components/CinemaAndTrends";
 import Navbar from "./components/Navbar";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div>
+      {" "}
+      <Navbar />
+      <CinemaAndTrends title="In Cinema Now" />
+      <CinemaAndTrends title="Trending Now" />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./moviecard.css";
+
+function MovieCard() {
+  return (
+    <div className="movie__card">
+      <img src="./img/dune.png" alt="poster of a movie" className="poster" />
+    </div>
+  );
+}
+
+export default MovieCard;
